5b7b202733b444f6986332420dfe07e7e7ccff2c
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-08-16 20:33:22 -0300

    pequeno ajuste

--- src/main/java/com/ourbabies/springboot/controller/HomeController.java
+++ src/main/java/com/ourbabies/springboot/controller/HomeController.java
@@ -24,95 +24,89 @@ import com.ourbabies.springboot.service.UsuarioService;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private ItemService itemService;
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private UsuarioRepository usuarioDAO;
-	
-	
+
 	@GetMapping("/home")
 	public String index(HttpSession session) {
-	//	session.setAttribute("usuario", "daniel");
+		// session.setAttribute("usuario", "daniel");
 		return "home";
 	}
-	
+
 	@GetMapping("/somos")
 	public String somos() {
 		return "somos";
 	}
-	
+
 	@GetMapping("/cadastro-usuario")
 	public String usuario() {
 		return "cadastro-usuario";
-	
+
 	}
-	
+
 	@GetMapping("/error")
-		public String errorPage() {
+	public String errorPage() {
 		return "redirect:/error";
 	}
-	
+
 	@GetMapping("/home-logado")
 	public String logado(Model model, HttpSession session) {
-		Usuario usuarioDados =  (Usuario) session.getAttribute("usuarioDados");
-		//System.out.println(usuarioDados);
+		Usuario usuarioDados = (Usuario) session.getAttribute("usuarioDados");
+		// System.out.println(usuarioDados);
 		List<Item> listaItens = itemService.getAllItem();
-		//System.out.println(listaItens);
-		if(usuarioDados != null) {
-		      model.addAttribute("usuarioSessao", usuarioDados);
-			  model.addAttribute("itens", listaItens);
-		      return "home-logado";
+		// System.out.println(listaItens);
+		if (usuarioDados != null) {
+			model.addAttribute("usuarioSessao", usuarioDados);
+			model.addAttribute("itens", listaItens);
+			return "home-logado";
 		} else {
 			return "redirect:/login";
 		}
-		
-	}
-	
 
-	
-	
+	}
 
 	@GetMapping("/esqueceu-senha")
 	public String recuperar() {
 		return "esqueceu-senha";
 	}
-	
-	
+
 	@PostMapping("/salvar-usuario")
-	public ModelAndView novoUsuario(String verificarSenha, @Valid @ModelAttribute(value = "Usuario") Usuario usuario, BindingResult result, Model model) {
-		//Verificação de e-mail cadastro no banco de dados,
-        ModelAndView modelAndView;
-        System.out.println(verificarSenha);
-        if (result.hasErrors()) {
-            modelAndView = new ModelAndView("cadastro-usuario");
-            modelAndView.addObject("usuario", usuario);
-            ArrayList<String> listaErros = new ArrayList<String>();
-            for (ObjectError objectError : result.getAllErrors()) {
-                listaErros.add(objectError.getDefaultMessage());
-            }
-            if(!usuario.getSenha().equals(verificarSenha)) {
-            	listaErros.add("A senha não combina");
-    		    }
-            modelAndView.addObject("msg", listaErros);
-            return modelAndView;
-        	  } else {
-              if (usuarioDAO.existsByCpf(usuario.getCpf())) {
-                  modelAndView = new ModelAndView("cadastro-usuario");
-                  return modelAndView;
-              }
-              //Verificação das senhas de cadastro
+	public ModelAndView novoUsuario(String verificarSenha, @Valid @ModelAttribute(value = "Usuario") Usuario usuario,
+			BindingResult result, Model model) {
+		// Verificação de e-mail cadastro no banco de dados,
+		ModelAndView modelAndView;
+		System.out.println(verificarSenha);
+		if (result.hasErrors()) {
+			modelAndView = new ModelAndView("cadastro-usuario");
+			modelAndView.addObject("usuario", usuario);
+			ArrayList<String> listaErros = new ArrayList<String>();
+			for (ObjectError objectError : result.getAllErrors()) {
+				listaErros.add(objectError.getDefaultMessage());
+			}
+			if (!usuario.getSenha().equals(verificarSenha)) {
+				listaErros.add("A senha não combina");
+			}
+			modelAndView.addObject("msg", listaErros);
+			return modelAndView;
+		} else {
+			if (usuarioDAO.existsByCpf(usuario.getCpf())) {
+				modelAndView = new ModelAndView("cadastro-usuario");
+				return modelAndView;
+			}
+			// Verificação das senhas de cadastro
 
-              usuarioService.save(usuario);
-              modelAndView = new ModelAndView("/login");
-              return modelAndView;
-            }
-
-	  }
+			usuarioService.save(usuario);
+			modelAndView = new ModelAndView("/login");
+			return modelAndView;
+		}
 
 	}
+
 }
